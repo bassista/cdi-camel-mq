62afd3a79539a467821902c717aefaca9db64e1a
peter.fry <pfry@redhat.com>
2016-10-19 16:58:15 +0100

    testing

--- src/main/java/org/pfry/Producers.java
+++ src/main/java/org/pfry/Producers.java
@@ -1,6 +1,9 @@
 package org.pfry;
 
 import org.apache.activemq.camel.component.ActiveMQComponent;
+import javax.enterprise.inject.*;
+import javax.enterprise.context *;
+import javax.inject.*;
 
 public class Producers {
  
