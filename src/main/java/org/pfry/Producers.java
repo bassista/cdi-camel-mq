17db7cf9bafbcd71cf4fd8dd7ffc59537e92d207
peter.fry <pfry@redhat.com>
2016-10-19 17:18:22 +0100

    testing

--- src/main/java/org/pfry/Producers.java
+++ src/main/java/org/pfry/Producers.java
@@ -13,9 +13,7 @@ public class Producers {
     @ApplicationScoped
     ActiveMQComponent getActiveMQComponent() {
         ActiveMQComponent component = new ActiveMQComponent();
-        component.setConnectionFactory(new ActiveMQConnectionFactory("failover://tcp://broker-amq-tcp.amq-demo.svc.cluster.local:61616"));
-        component.setUserName("amq-demo-user");
-        component.setPassword("password");
+        component.setConnectionFactory(new ActiveMQConnectionFactory("amq-demo-user", "password", "failover://tcp://broker-amq-tcp.amq-demo.svc.cluster.local:61616"));
         return component;
     }
 }
